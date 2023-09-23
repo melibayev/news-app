7b9ee1b2f87feb4643faa1d4e64a359d62747485
“melibayev” <melibayefff1@gmail.com>
2023-09-23 06:05:43 +0500

    name and logo are changed

--- src/components/latestNews/LatestNews.jsx
+++ src/components/latestNews/LatestNews.jsx
@@ -3,7 +3,12 @@ import { NavLink } from 'react-router-dom'
 import styles from './LatestNews.module.scss'
 
 const LatestNews = ({media, updated, title, source, id}) => {
-  const img = media[0]['media-metadata'][2].url
+  let img
+  if (media === false || media.length == 0) {
+    img = 'https://i.stack.imgur.com/y9DpT.jpg'
+  } else {
+    img = media[0]['media-metadata'][2].url
+  }
   return (
     <NavLink to={`/news-detail/${id}`}>
         <div className={styles.card}>
