e09c68ef672113c227b60524a1c166b01bfa1c62
“melibayev” <melibayefff1@gmail.com>
2023-09-23 21:27:51 +0500

    final commit

--- src/components/popularNewsCard/PopularNewsCard.jsx
+++ src/components/popularNewsCard/PopularNewsCard.jsx
@@ -3,14 +3,12 @@ import { NavLink } from 'react-router-dom';
 import styles from './PopularNewsCard.module.scss';
 
 const Popular_news_card = ({media, updated, title, source, id}) => {
-    let img = media[0]['media-metadata'][2].url
-    // if (media === false) {
-    //     console.log(media);
-    // }
-    // console.log(id, media); 
-    if (media === false || media[0]['media-metadata'] === false || media.length === 0) {
+    let img
+    if (media === false || media.length == 0) {
         img = 'https://i.stack.imgur.com/y9DpT.jpg'
-      }
+    } else {
+        img = media[0]['media-metadata'][2].url
+    }
     return (
     <NavLink to={`/news-detail/${id}`}>
         <div className={styles.card}>
